dd0e9aaa52e6e10cba4deff6b1118706e40ec287
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-02-06 02:10:55 -0300

    Atualizando arquivo admin_login_template.php e corrigindo bugs, adicionando verifição condicional para inicio de sessão com session_start();

--- 02-admin/admin_home.php
+++ 02-admin/admin_home.php
@@ -3,7 +3,10 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // verifica se o usuário está autenticado.
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
     header("Location: admin_login.php");
     exit();
